9fb98cdde3b43d0bd54bf722e2cc595851214a05
Gabriel Anguiano <anguianog97@gmail.com>
2020-01-15 13:16:59 -0800

    router is done, almost done with post routes

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,17 +1,63 @@
 const express = require('express');
 
+const postsDataBase = require('./postDb')
+
+
+
 const router = express.Router();
+
+
 
 router.get('/', (req, res) => {
   // do your magic!
+   postsDataBase.get()
+   .then( item => {
+     res.status(201).json(item)
+   })
+   .catch(error => {
+     res.status(500).json({error: "Something went wrong in the database"})
+   })
+
 });
 
-router.get('/:id', (req, res) => {
+ function validId( req, res, next){
+    
+
+    postsDataBase.getById(req.params.id)
+    .then(result => {
+        if(result){
+          req.result = result.id
+          next();
+        } else {
+          res.status(404).json({message: "Specified Id does not exist"})
+        }
+    })
+ }
+
+router.get('/:id', validId , (req, res) => {
   // do your magic!
+
+   
+  postsDataBase.getById(req.result)
+   .then( result => {
+     res.status(201).json(result)
+   }) 
+   .catch(error => {
+     res.status(500).json({errorMessage: "Error with server"})
+   })
+
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id' ,validId, (req, res) => {
   // do your magic!
+   postsDataBase.remove(req.result)
+   .then( result => {
+     res.status(201).json(4)
+   })
+   .catch(error => {
+     res.status(500).json({error: "Something is wrong in database"})
+   })
 });
 
 router.put('/:id', (req, res) => {
@@ -20,8 +66,6 @@ router.put('/:id', (req, res) => {
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
-}
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 
+const postRoutes = require('./posts/postRouter')
+
+const userRoutes = require('./users/userRouter')
+
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json())
+
+server.use('/api/posts', postRoutes)
+
+server.get('/', logger , (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {}
+ function logger(req, res, next) {
+ const { method , originalUrl } = req;
+
+ console.log(`${method} to ${originalUrl}`)
+
+ next();
+
+}
 
 module.exports = server;
